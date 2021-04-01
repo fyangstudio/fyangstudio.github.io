c3dd65be1a8c5c02986d03bc5873660c1538d6fa
fyangstudio <18767120422@163.com>
2021-04-01 03:22:37 +0000

    Deploying to master from @ fyangstudio/fyangstudio.github.io@a5eebe83c7bc3cf99b7384e43b9129c877abaf96 🚀

--- docs/.vuepress/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  title: 'Iceworks',
-  markdown: {
-    lineNumbers: true,
-    extendMarkdown: md => {
-      md.use(require('markdown-it-multimd-table'))
-    }
-  }
-}
--- docs/README.md
+++
@@ -1,3 +0,0 @@
-# Hello Iceworks
-
-[start](./start.md)
